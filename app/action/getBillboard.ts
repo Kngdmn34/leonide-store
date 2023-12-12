75bbd71d864eb60d2c0fc05595a861413eaa58d7
kng.dmn34 <96668023+Kngdmn34@users.noreply.github.com>
2023-12-12 17:49:19 +0100

    seo opt

--- app/action/getBillboard.ts
+++ app/action/getBillboard.ts
@@ -1,25 +1,30 @@
-import React from 'react';
-import axios, {AxiosResponse} from 'axios';
-import { Billboard} from '@/types'
+import { Billboard } from '@/types';
+import axios, { AxiosResponse } from 'axios';
 
-const URL = `${process.env.NEXT_PUBLIC_API}/billboard`
+const URL = `${process.env.NEXT_PUBLIC_API}/billboard`;
 
-const getBillboard =async (): Promise<Billboard[]> => {
-
-    try  {
-        const res: AxiosResponse<{allbillboards: Billboard[]} > = await axios.get(URL)
-        if(res.data && res.data.allbillboards){ 
-            console.log(res.data);
-            return res.data.allbillboards
-        }
-        return []
-
-
+const getBillboard = async (): Promise<Billboard[]> => {
+  try {
+    const res: AxiosResponse<{ allbillboards: Billboard[] }> = await axios.get(URL);
+    if (res.data && res.data.allbillboards) {
+      console.log(res.data);
+      return res.data.allbillboards;
     }
-  catch(error) { 
-    console.log('ERROR FETCHING BILLBOARD DATA', error)
-    throw error
+    return [];
+  } catch (error) {
+    console.error('ERROR FETCHING BILLBOARD DATA', error);
+    throw error;
   }
-}
+};
 
-export default getBillboard
+export default getBillboard;
+
+export async function getServerSideProps() {
+  const data = await getBillboard();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
